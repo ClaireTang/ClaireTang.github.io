a367cddce3d947adda4cdb18f10a7f277dcbb0c9
claireTang <1031352394@qq.com>
2018-05-18 17:25:38 +0800

    引入图片

---
+++ data.js
@@ -0,0 +1,23 @@
+const data_0 = [
+	{
+		"url": "images/0/1.jpg"
+	},{
+		"url": "images/0/2.jpg"
+	},{
+		"url": "images/0/3.jpg"
+	}
+]
+const data_1 = [
+	{
+		"url": "images/1/1.jpg"
+	},{
+		"url": "images/1/2.jpg"
+	},{
+		"url": "images/1/3.jpg"
+	}
+]
+
+module.exports = {
+  data_0,
+  data_1
+}
